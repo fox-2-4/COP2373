3d5e4e288ee05c1cdf923f3ced587da1288c0862
Brayden Fox <bmfox98@gmail.com>
2025-05-14 18:36:48 -0400

    Final version

--- BraydenFox_ProgrammingExercise_1.py
+++ BraydenFox_ProgrammingExercise_1.py
@@ -1,35 +1,57 @@
+# COP 2373
+# Brayden Fox
+# 05/14/25
 
 
+# main program
 def main():
+    # unchanging program parameters
     TOTAL_NOF_TICKETS = 20
     MAX_TICKETS_PER_BUYER = 4
 
+    # stores the current number of remaining tickets available for purchase
     curr_nof_tickets = TOTAL_NOF_TICKETS
+
+    # stores the quantity purchased by each buyer (the length of the array "stores" the number of buyers)
     qty_per_buyer: list = []
 
+    # main loop
     while curr_nof_tickets > 0:
 
-        curr_qty = 0
+        # calculate the current maximum number of tickets remaining to be sold on this iteration
         curr_max_qty = min(MAX_TICKETS_PER_BUYER, curr_nof_tickets)
 
+        # loop for input & validation
         while True:
             print(f"Tickets remaining: {curr_nof_tickets}")
-            curr_qty = input("> Enter the desired number of tickets to purchase: ")
-            if not curr_qty.isdigit():
-                print("Invalid input. Please enter a valid number.")
-                continue
-            curr_qty = int(curr_qty)
+            curr_qty = collect_int_from_user()
+
+            # confirm chosen quantity lies within the current acceptable range
             if curr_qty == 0 or curr_qty > curr_max_qty:
                 print(f"Invalid amount. Please enter a nonzero amount less than or equal to [{curr_max_qty}].")
                 continue
             break
 
+        # append the chosen amount to the quantity-per-buyer array and modify the current number of tickets
         qty_per_buyer.insert(-1, curr_qty)
         curr_nof_tickets -= curr_qty
 
+    # all tickets have been sold; display final output & exit
     print("All tickets have been sold.")
     print(f"Total number of buyers: {len(qty_per_buyer)}.")
 
 
+# collects and validates user input as a digit string
+def collect_int_from_user() -> int:
+    while True:
+        qty = input("> Enter the desired number of tickets to purchase: ")
+        if not qty.isdigit():
+            print("Invalid input. Please enter a valid number.")
+        else:
+            break
+    return int(qty)
+
+
+# calls the main program
 if __name__ == "__main__":
     main()
